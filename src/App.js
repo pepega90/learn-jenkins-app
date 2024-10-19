cfb6663994d098cad176d0143a4c62cb14b04c5e
pepega90 <asuarif5@gmail.com>
2024-10-19 20:55:29 +0700

    c

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          anjay pak tua jenkins
         </a>
       </header>
       <p>
